0106ad4359040914e1b2e391330f84e718c32966
Riio <superrio25@gmail.com>
2020-08-04 22:10:32 -0600

    added carousel that doesnt work

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="container view">
+    <div class="container-fluid">
         <div class="top-bar">
             <span>Hamburger</span>
             <span>Vegan-Amazon</span>
@@ -24,14 +24,30 @@
                 </div>
             </div>
         </div>
-        <div>
-            <div>
-                <h1>Carosel goes here</h1>
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://source.unsplash.com/1600x900/?clouds,water" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://source.unsplash.com/1600x900/?ocean,water" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://source.unsplash.com/1600x900/?trees,water" alt="Third slide">
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
 
-        <div id="myBoxes">
-            <div id="box1" class="boxes4 col-sm-2">
+        <div class="row">
+            <div class="col-sm-3">
                 <div class="header">
                     <h2>Box 1 Header</h2>
                 </div>
@@ -40,7 +56,7 @@
                 </div>
             </div>
 
-            <div id="box2" class="boxes4 col-sm-2">
+            <div class="col-sm-3">
                 <div class="header">
                     <h2>Box 2 Header</h2>
                 </div>
@@ -48,7 +64,7 @@
                     <p class="bodies">the body of this box goes here</p>
                 </div>
             </div>
-            <div id="box3" class="boxes4 col-sm-2">
+            <div class="col-sm-3">
                 <div class="header">
                     <h2>Box 3 Header</h2>
                 </div>
@@ -56,7 +72,7 @@
                     <p class="bodies">the body of this box goes here</p>
                 </div>
             </div>
-            <div id="box4" class="boxes4 col-sm-2">
+            <div class="col-sm-3">
                 <div class="header">
                     <h2>Box 4 Header</h2>
                 </div>
